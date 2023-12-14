cb8e565d863781e097b9859abd92a81289869db2
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2023-12-14 16:27:05 -0500

    update 1

--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -2,16 +2,32 @@
    <header class="sticky top-2 bg-weather-primary shadow-lg">
     <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
 
-    <div class="flex items-center gap-3 flex-1">
-    <i class="fa-solid fa-sun text-2xl"></i>
-    <p class="text-2x1">
-    The local Weather
-    </p>
+    <RouterLink :to="{name:'home'}">
+        <div class="flex items-center gap-3">
+        <i class="fa-solid fa-sun text-2xl"></i>
+        <p class="text-2x1">
+        The local Weather
+        </p>
     </div>
+</RouterLink>
+
+    <div class="flex gap-3 flex-1 justify-end"> 
+       <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+       <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+
+    </div>
+
+   
     </nav>
+    
    </header>
+
+   <BaseModal :modalActive="modalActive"  @close-modal="toggleModal">
+    <h1 class="text-black">Hello From Modal</h1>
+    </BaseModal>
 </template>
 
 <script setup>
-
+import { RouterLink } from 'vue-router';
+import BaseModal from './BaseModal.vue';
 </script>
