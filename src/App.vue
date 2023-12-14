449b6c661b94170a6098fab6a7f790d55d870970
IsaiNah <38513474+IsaiNah@users.noreply.github.com>
2023-12-14 14:44:55 -0500

    Added my project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+<SiteNav />
+<RouterView />
+
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+import SiteNav from './components/SiteNav.vue';
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SiteNav.vue
@@ -0,0 +1,17 @@
+<template>
+   <header class="sticky top-2 bg-weather-primary shadow-lg">
+    <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+
+    <div class="flex items-center gap-3 flex-1">
+    <i class="fa-solid fa-sun text-2xl"></i>
+    <p class="text-2x1">
+    The local Weather
+    </p>
+    </div>
+    </nav>
+   </header>
+</template>
+
+<script setup>
+
+</script>
